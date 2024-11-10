633f5503986d7de11566173ca155be16ca137094
cdwhite2 <cdwhite@protonmail.com>
2024-11-10 19:57:56 +0000

    project complete

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,15 @@
+class MoviesController < ApplicationController
+  validates(:director_id, presence: true)
+  
+  def index
+    @list_of_movies = Movie.all
+
+    render({ :template => "pages_templates/movies" })
+  end
+
+  def details
+    @movie = Movie.where({ :id => params.fetch("movie_id") }).first
+
+    render({ :template => "pages_templates/movie_details"})
+  end
+end
